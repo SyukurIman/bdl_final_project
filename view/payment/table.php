4390919f09d95e3b68442ee4f234553b36ec148b
candrakmb <candrakmb07@gmail.com>
2023-12-15 19:40:29 +0700

    filter top donatur

--- view/payment/table.php
+++ view/payment/table.php
@@ -13,6 +13,7 @@
           <div class="col-lg-6 col-5 my-auto text-end">
             <div class="dropdown float-lg-end pe-4">
               <button class="btn btn-secondary btn-data" id="filter_btn"><i class="fas fa-filter"></i> <span>Filter</span></button>
+              <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#top_donasi"><i class="fas fa-trophy"></i> <span> Top Donasi Filter</span></button>
               <button class="btn btn-warning btn-data" id="download_btn"><i class="fas fa-download"></i> <span>Download</span></button>
           </div>
           </div>
@@ -84,3 +85,31 @@
       </div>
     </div>
   </div>
+
+  
+<div class="modal fade" id="top_donasi" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Top Donatur Berdasarkan Tanggal</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form method="post" id="form_top_donasi">
+          <div class="mb-3">
+            <label for="tanggal_awal" class="col-form-label">tanggal Awal</label>
+            <input type="date" class="form-control" id="tanggal_awal">
+          </div>
+          <div class="mb-3">
+            <label for="tanggal_akhir" class="col-form-label">Tanggal Akhir</label>
+            <input type="date" class="form-control" id="tanggal_akhir">
+          </div>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary"  data-bs-dismiss="modal" id="submit_top_donasi">Submit</button>
+      </div>
+    </div>
+  </div>
+</div>
